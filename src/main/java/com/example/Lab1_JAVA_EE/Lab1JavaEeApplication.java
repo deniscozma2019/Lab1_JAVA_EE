a2de61e1ba32c776028257d8afe990fc5e73b858
denicozma2019 <denis.cozma2015@gmail.com>
2023-03-01 13:43:15 +0200

    Initial commit

---
+++ src/main/java/com/example/Lab1_JAVA_EE/Lab1JavaEeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lab1_JAVA_EE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1JavaEeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1JavaEeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Lab1_JAVA_EE/Lab1JavaEeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lab1_JAVA_EE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1JavaEeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
